d47a3c195f3b3a9f5eda31c83de2abfe179b94a4
RohitM2019 <rohitmittal@sfhs.com>
2018-03-23 23:07:38 -0700

    Cases for opposite scale

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -234,6 +234,7 @@ class Robot : public frc::IterativeRobot
           rMotionProfile->startFilling(motionProfile_StraightLine_right, count_StraightLine_right);
           lMotionProfile->start();
           rMotionProfile->start();
+          position = SmartDashboard::GetString ("Starting Position (LEFT, RIGHT, CENTER)", "LEFT");
           /*
             DriverStation::ReportError ("AutonInit Started");
 
@@ -273,8 +274,9 @@ class Robot : public frc::IterativeRobot
               _lMotorFront->GetMotionProfileTopLevelBufferCount() +
 	      _rMotorFront->GetMotionProfileTopLevelBufferCount() == 0)
 	      {
-		depositingCube = true;
-		timeDepositingCube = Timer().GetFPGATimestamp();
+		if((position == "LEFT" && gameData == "L") || (position == "RIGHT" && gameData == "R"))
+		  depositingCube = true;
+		  timeDepositingCube = Timer().GetFPGATimestamp();
 	      }
           if(depositingCube)
 	    {
